2c53b82279494bb013dad0593208e85c00690056
mannerisms <mannerisms@gmail.com>
2016-04-03 19:13:26 +0200

    New: Profile Switcher

--- cda/urls.py
+++ cda/urls.py
@@ -6,6 +6,14 @@ app_name = 'cda'
 urlpatterns = [
     # /cda/
     url(r'^$', views.index, name='index'),
+    # /cda/sources/persons/
+
     url(r'^sources/persons/$', views.persons, name='persons'),
+    # /cda/sources/persons/#
+
+    # url(r'^sources/persons/profile/', views.person_profile, name='persons_profile'),
+    url(r'^sources/persons/(?P<person_id>[0-9]+)/$', views.person_profile, name='persons_profile'),
+
+    # /cda/map/
     url(r'^map/$', views.map_view, name='map_view'),
 ]
--- cda/views.py
+++ cda/views.py
@@ -11,5 +11,11 @@ def persons(request):
     return render(request, 'cda/persons.html', {'all_persons': all_persons})
 
 
+def person_profile(request, person_id):
+    get_object_or_404(Person, pk=person_id)
+    person = Person.objects.get(pk=person_id)
+    return render(request, 'cda/person_profile.html', {'person': person})
+
+
 def map_view(request):
     return render(request, 'cda/map_view.html')
